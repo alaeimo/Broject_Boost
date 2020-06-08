926694fe07a1878f5dffcd03588fca2230fd017b
Mohammad Alaei <alaei.py@gmail.com>
2020-06-08 20:32:51 +0430

    Rocket and rocket control script created for first time!

---
+++ Assets/Rocket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float rcsTrust = 5f;
+    [SerializeField] float mainTrust = 20f;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up * mainTrust);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+            transform.Rotate(Vector3.forward * rcsTrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+            transform.Rotate(-Vector3.forward * rcsTrust);
+        }
+        rigidBody.freezeRotation = false;
+    }
+}
